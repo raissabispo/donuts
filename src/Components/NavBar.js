9ca79c7d524f7a7535f086febe58ddde7239b7f9
Raissa <uamr.ieadpe1@gmail.com>
2025-03-26 21:24:00 -0300

    adiciona ícones do React e  atualiza estilos da NavBar

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,17 +1,24 @@
+import React from "react";
 import Logo from "../Assets/Logo.svg";
+import { BsCart2 } from "react-icons/bs";
 
 const NavBar = () => {
   return (
-    <div>
-      <div className="nav-logo-container"> 
-        <img src={Logo || ""} alt="logo donuts" /> {/* Garante que src não seja vazio */}
+    <nav>
+      <div className="nav-logo-container">
+        <img src={Logo || ""} alt="logo donuts" />
       </div>
-       <div className="nav-menu-container">
-           <a href="/">Home</a>
-           
-       </div>
-    </div>
-    
+      <div className="nav-menu-container">
+        <a href="/">Home</a>
+        <a href="/">Sobre nós</a>
+        <a href="/">Nosso trabalho</a>
+        <a href="/">Avaliação</a>
+        <a href="/">Contato</a>
+        <a href="/">
+          <BsCart2 className="navbar-cart-icon" />
+        </a>
+      </div>
+    </nav>
   );
 };
 
